56ed6efeee45c71c3c2419a838e3e7ba1e669420
hameetha <hameethasanofer610@gmail.com>
2024-05-16 19:05:52 +0530

    second commit

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "green":"#39DB4A",
+        "red":"#FF6868",
+        "secondary":"#555",
+        "primaryBG":"#FCFCFC",
+    },
+    fontFamily:{
+      "primary":['Inter', 'sans-serif']
+      
+    }
+
+    },
+  },
+  plugins: [],
+}
